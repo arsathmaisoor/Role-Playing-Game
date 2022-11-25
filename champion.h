f65700f9a7607cbba455b7730bee8ab812e8071d
Arsath99 <90707804+Arsath99@users.noreply.github.com>
2022-11-25 11:46:08 +0530

    Task 1 completed

--- champion.h
+++ champion.h
@@ -1,6 +1,18 @@
-struct Champion
+#ifndef CHAMPION //Header guard
+#define CHAMPION
+//enum creation for Champ roles
+enum ChampionRole{MAGE,FIGHTER,SUPPORT,TANK};
+
+//Champion
+struct Champion 
 {
     
     int level;
-    Champion next;
+    struct Champion* next; //node for linked list
+    Champion* createChampion();
+    Champion* addChampion(Champion *head, Champion *c);
+    Champion* buildChampionList(int n);
+    void printChampionList(Champion *head); 
 };
+
+#endif
